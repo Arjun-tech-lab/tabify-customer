757785630ff06ae562e3ac776b0ebbf713c4d500
Arjun Indavara <arjunindavara@Arjuns-MacBook-Air.local>
2025-11-02 15:20:34 +0530

    socket.js changed

--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,10 @@
 // utils/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5001", {
-  transports: ["websocket"], // helps maintain stable connection
-});
+export const socket = io(
+  process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "http://localhost:5001",
+  {
+    transports: ["websocket"],
+    withCredentials: true,
+  }
+);
